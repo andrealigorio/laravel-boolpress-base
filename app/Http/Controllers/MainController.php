10c08248829e6784613175ac1cef8d629dcde951
Andrea Ligorio <andrea85kro@gmail.com>
2021-05-18 15:15:58 +0200

    Creazione model migrations controller resources e seeder

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        return view('home');
+    }
+
+}
